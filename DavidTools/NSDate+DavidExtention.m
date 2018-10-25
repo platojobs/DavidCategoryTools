233a4e9ffef5e783d253010fcc6a98ff0dcc9ff6
yangwei cui <cuishengxisvip@gmail.com>
2018-10-25 16:46:08 +0800

    日期转化

---
+++ DavidTools/NSDate+DavidExtention.m
@@ -0,0 +1,188 @@
+//
+//  NSDate+DavidExtention.m
+//  David_ObjectiveC
+//
+//  Created by MOON FLOWER on 2018/10/25.
+//  Copyright © 2018 David. All rights reserved.
+//
+
+#import "NSDate+DavidExtention.h"
+
+@implementation NSDate (DavidExtention)
+
+
++ (NSString *)timeStringWithTimeInterval:(NSString *)timeInterval
+{
+    NSDate *date = [NSDate dateWithTimeIntervalSince1970:timeInterval.longLongValue/1000]; //此处根据项目需求,选择是否除以1000 , 如果时间戳精确到秒则去掉1000
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    
+    //今天
+    if ([date isToday]) {
+        
+        formatter.dateFormat = @"HH:mm";
+        
+        return [formatter stringFromDate:date];
+    }else{
+        
+        //昨天
+        if ([date isYesterday]) {
+            
+            formatter.dateFormat = @"昨天 HH:mm";
+            return [formatter stringFromDate:date];
+            
+            //一周内 [date weekdayStringFromDate]
+        }else if ([date isSameWeek]){
+            
+            formatter.dateFormat = [NSString stringWithFormat:@"%@ %@",[date weekdayStringFromDate],@"HH:mm"];
+            return [formatter stringFromDate:date];
+            
+            //直接显示年月日
+        }else{
+            
+            formatter.dateFormat = @"yy-MM-dd  HH:mm";
+            return [formatter stringFromDate:date];
+        }
+    }
+    return nil;
+}
+
+//是否在同一周
+- (BOOL)isSameWeek
+{
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    int unit = NSCalendarUnitWeekday | NSCalendarUnitMonth | NSCalendarUnitYear ;
+    NSDateComponents *nowCmps = [calendar components:unit fromDate:[NSDate date]];
+    NSDateComponents *selfCmps = [calendar components:unit fromDate:self];
+    return (selfCmps.year == nowCmps.year) && (selfCmps.month == nowCmps.month) && (selfCmps.day == nowCmps.day);
+}
+
+
+//根据日期求星期几
+- (NSString *)weekdayStringFromDate{
+    
+    NSArray *weekdays = [NSArray arrayWithObjects: [NSNull null], @"星期天", @"星期一", @"星期二", @"星期三", @"星期四", @"星期五", @"星期六", nil];
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSTimeZone *timeZone = [[NSTimeZone alloc] initWithName:@"Asia/Shanghai"];
+    [calendar setTimeZone: timeZone];
+    NSCalendarUnit calendarUnit = NSCalendarUnitWeekday;
+    NSDateComponents *theComponents = [calendar components:calendarUnit fromDate:self];
+    return [weekdays objectAtIndex:theComponents.weekday];
+}
+
+
+//是否为今天
+- (BOOL)isToday
+{
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    int unit = NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear ;
+    NSDateComponents *nowCmps = [calendar components:unit fromDate:[NSDate date]];
+    NSDateComponents *selfCmps = [calendar components:unit fromDate:self];
+    return (selfCmps.year == nowCmps.year) && (selfCmps.month == nowCmps.month) && (selfCmps.day == nowCmps.day);
+}
+
+
+
+//是否为昨天
+- (BOOL)isYesterday
+{
+    
+    NSDate *nowDate = [[NSDate date] dateWithYMD];
+    NSDate *selfDate = [self dateWithYMD];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *cmps = [calendar components:NSCalendarUnitDay fromDate:selfDate toDate:nowDate options:0];
+    return cmps.day == 1;
+}
+
+//格式化
+- (NSDate *)dateWithYMD
+{
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    fmt.dateFormat = @"yyyy-MM-dd";
+    NSString *selfStr = [fmt stringFromDate:self];
+    return [fmt dateFromString:selfStr];
+}
+
+//获取当前的时间
+-(NSString*)getCurrentTime:(NSString*)fomatString {
+    
+    NSDateFormatter*formatter = [[NSDateFormatter alloc]init];[formatter setDateFormat:fomatString];
+    
+    NSString*dateTime = [formatter stringFromDate:[NSDate date]];
+    
+    return dateTime;
+    
+}
+//获取当前时间戳
+-(NSString*)getCurrentTimestamp{
+    
+    NSDate* dat = [NSDate dateWithTimeIntervalSinceNow:0];
+    
+    NSTimeInterval a=[dat timeIntervalSince1970];
+    
+    NSString*timeString = [NSString stringWithFormat:@"%0.f", a];//转为字符型
+    
+    return timeString;
+    
+}
+// 获取当前时间戳  系统的时间戳  10位的
+- (NSString *)getNowTime{
+    // 获取时间（非本地时区，需转换）
+    NSDate * today = [NSDate date];
+    NSTimeZone *zone = [NSTimeZone systemTimeZone];
+    NSInteger interval = [zone secondsFromGMTForDate:today];
+    // 转换成当地时间
+    NSDate *localeDate = [today dateByAddingTimeInterval:interval];
+    // 时间转换成时间戳
+    NSString *timeSp = [NSString stringWithFormat:@"%ld",(long)[localeDate timeIntervalSince1970]];//@"1517468580"
+    return timeSp;
+}
+//时间戳转化为字符串
+-(NSString*)stampTimeFomat:(NSString*)time{
+    
+    NSDateFormatter*formatter = [[NSDateFormatter alloc]init];
+    
+    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    
+    [formatter setTimeStyle:NSDateFormatterShortStyle];
+    
+    [formatter setDateFormat:@"yyyMMdd"];
+    
+    int timeval = [time intValue];
+    
+    NSDate*confromTimesp = [NSDate dateWithTimeIntervalSince1970:timeval];
+    
+    // NSLog(@"1296035591  = %@",confromTimesp);
+    
+    NSString*confromTimespStr = [formatter stringFromDate:confromTimesp];
+    
+    return confromTimespStr;
+    
+}
+//时间戳转化为时间然后与当前时间减得出剩余时间
+-(NSString*)getTimeStringWithSp:(NSString *)sp{
+    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    formatter.timeZone = [NSTimeZone timeZoneWithName:@"beijing"];
+    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    [formatter setTimeStyle:NSDateFormatterShortStyle];
+    [formatter setDateFormat:@"MM月dd日"];
+    //[formatter setDateFormat:@"yyyy年MM月dd日 HH:mm:ss"];
+    NSDate* endDate = [NSDate dateWithTimeIntervalSince1970:sp.doubleValue];
+    
+    NSDate*nowDate=[NSDate date];
+    
+    NSCalendar*gregorian = [[NSCalendar alloc]initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    unsigned int unitFlags = NSCalendarUnitDay;
+    NSDateComponents*comps = [gregorian components:unitFlags fromDate:nowDate toDate:endDate options:0];
+    
+    NSString* dateString = [formatter stringFromDate:endDate];
+    NSLog(@"月日:%@",dateString);
+    return [NSString stringWithFormat:@"%ld",(long)[comps day]];
+    
+    
+    
+    
+    
+}
+
+
+@end
